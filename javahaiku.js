89b8a9566cb6fcf52c30d1460ff3a3fc33710d0b
Nate Smith <nate.smith@coxinc.com>
2011-09-19 16:06:05 -0400

    initial commit

---
+++ javahaiku.js
@@ -0,0 +1,17 @@
+// run, produce a haiku.
+
+var API = "https://www.google.com/codesearch/feeds/search";
+var STARTMAX = 2000;
+var start_index = Math.floor(Math.random()*STARTMAX);
+
+var fives = [];
+var sevens = [];
+
+// pick a random start-index less than STARTMAX
+// start walking, looking for java class name: need two 5s and 7
+
+// ...
+
+// shuffle two fives around 7
+// print result
+
