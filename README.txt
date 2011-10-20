e73ecb58225191b6f859aa97c88464ea118b2b31
Nate Smith <nate.smith@coxinc.com>
2011-10-20 18:15:33 -0400

    add readme and license

---
+++ README.txt
@@ -0,0 +1,22 @@
+this program generates haiku from java class names.
+dump .java files into java/
+get haiku.
+
+note: this program is slow, inefficient, undocumented, and a
+hacked-together-in-20-minutes-art-project. so, enjoy.
+
+ActionMessageTag
+DependencyException
+TwoResultAction
+
+StrutsPrepareFilter
+DoubleSelectDirective
+CreateException
+
+RadioModel
+StrutsSpringObjectFactory
+WildcardHelperTest
+
+ScopeInterceptor
+ActionInvocationTest
+ListPeopleAction
--- javahaiku.js
+++ javahaiku.js
@@ -1,5 +1,20 @@
 #!/usr/bin/env node
 
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 var events = require('events');
 var fs = require('fs');
 var sys = require('sys');
@@ -49,6 +64,3 @@ var five_index_two = Math.floor(Math.random() * fives.length+1);
 console.log(fives[five_index_one]);
 console.log(sevens[seven_index]);
 console.log(fives[five_index_two]);
-
-
-
